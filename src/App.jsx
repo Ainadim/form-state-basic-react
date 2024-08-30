3f8b8b709631c9f15655521fd61da9372bdc64b7
unknown <nadimface@gmailcom>
2024-08-30 21:53:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [nameInput, setNameInput] = useState()
+  const [mail, setMailInput] = useState()
+  const [password, setPassword] = useState()
+
+  const [error, setError] = useState()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (password.length < 6 ) {
+      setError("Please provide the password with 6 or more character ")
+    }
+    else{
+      console.log(nameInput, mail, password);
+    }
+
+    
+  }
+
+  const handleNameinput = (e) => {
+    console.log(e.target.value);
+    setNameInput(e.target.value)
+  }
+
+  const handleEmailInput = (e) => {
+    console.log(e.target.value);
+    setMailInput(e.target.value);
+  }
+
+  const handlePassword = (e) => {
+    console.log(e.target.value);
+    setPassword(e.target.value)
+  }
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <form onSubmit={handleSubmit} action="">
+        <input onChange={handleNameinput} type="text" name='Name' />
+        <br />
+        <input onChange={handleEmailInput} required type="email" name='Email' />
+        <br />
+        <input onChange={handlePassword} required type="password" name="Password" required />
+        <br />
+        <input type="submit" value="Submit" />
+
+        {
+          error && <p>{error}</p>
+        }
+      </form>
+    </>
+  )
+}
+
+export default App
